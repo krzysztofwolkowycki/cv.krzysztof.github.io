1dc047d5fb19d664b89da1a98758e560b20d4910
Krzysztof Wolkowycki <wolkowycki.k@gmail.com>
2017-12-18 19:15:01 +0100

    Out base.css

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-console.log('hello');
+function bigImg(x) {
+    x.style.height = "200px";
+    x.style.width = "200px";
+}
+
+function normalImg(x) {
+    x.style.height = "150px";
+    x.style.width = "150px";
+}
